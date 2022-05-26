5978cd6a64545d48aa5e5648f38ce3bda11d6171
ujin-Bang <qkddnwls14@gmail.com>
2022-05-26 15:13:10 +0900

    리스트뷰 배치 + 방 한줄 xml 작성

--- app/src/main/java/com/start/jickbangcopy_20220526/MainActivity.kt
+++ app/src/main/java/com/start/jickbangcopy_20220526/MainActivity.kt
@@ -2,10 +2,15 @@ package com.start.jickbangcopy_20220526
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.start.jickbangcopy_20220526.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
     }
 }
