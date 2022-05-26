777cc04700656456d11c462393d0ce4036afde84
ujin-Bang <qkddnwls14@gmail.com>
2022-05-26 17:08:54 +0900

    방 상세 화면 UI작업

---
+++ app/src/main/java/com/start/jickbangcopy_20220526/ViewRoomDetatilActivity.kt
@@ -0,0 +1,11 @@
+package com.start.jickbangcopy_20220526
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewRoomDetatilActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_room_detatil)
+    }
+}
