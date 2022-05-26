8767fb3148435e3319e28b7cdfd9cedc19a68084
ujin-Bang <qkddnwls14@gmail.com>
2022-05-26 15:46:50 +0900

    어댑터 변수 객체화 + 리스트뷰의 어댑터로 연결

--- app/src/main/java/com/start/jickbangcopy_20220526/MainActivity.kt
+++ app/src/main/java/com/start/jickbangcopy_20220526/MainActivity.kt
@@ -3,6 +3,7 @@ package com.start.jickbangcopy_20220526
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import com.start.jickbangcopy_20220526.adapters.RoomAdapter
 import com.start.jickbangcopy_20220526.databinding.ActivityMainBinding
 import com.start.jickbangcopy_20220526.datas.RoomData
 
@@ -10,6 +11,9 @@ class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
     val mRooms = ArrayList<RoomData>()
+
+    lateinit var mRoomAdapter: RoomAdapter
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,5 +24,9 @@ class MainActivity : AppCompatActivity() {
         mRooms.add(RoomData(29700, "경기도 고양시", 17, "3번째 방입니다."))
         mRooms.add(RoomData(43000, "서울시 중구", -2, "4번째 방입니다."))
         mRooms.add(RoomData(175300, "서울시 송파구", 20, "5번째 방입니다."))
+
+        mRoomAdapter = RoomAdapter(this,R.layout.room_list_item, mRooms)
+        binding.roomListView.adapter = mRoomAdapter
+
     }
 }
