cfae807ebb87c91d5bd5118a6e219002345a30be
ujin-Bang <qkddnwls14@gmail.com>
2022-05-26 14:57:22 +0900

    Initial commit

---
+++ app/src/main/java/com/start/jickbangcopy_20220526/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.start.jickbangcopy_20220526
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
