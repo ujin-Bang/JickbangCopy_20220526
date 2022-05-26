7202b31f83f8c74531b6a774e0bd19ae68742768
ujin-Bang <qkddnwls14@gmail.com>
2022-05-26 19:44:32 +0900

    Handler / postDelayed 함수 활용  - 2.5초 후에 메인으로 이동하도록  - 로딩화면 종료

--- app/src/main/java/com/start/jickbangcopy_20220526/SplashActivity.kt
+++ app/src/main/java/com/start/jickbangcopy_20220526/SplashActivity.kt
@@ -1,11 +1,29 @@
 package com.start.jickbangcopy_20220526
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+
+//        2.5초 이후에 넘어가게 하려면? => Handler 클래스 활용
+        val myHandler = Handler(Looper.getMainLooper())
+
+        myHandler.postDelayed({
+//            이 중괄호 내부의 코드는, 2.5초 후에 실행될 예정
+            val myIntent = Intent(this, MainActivity::class.java)
+            startActivity(myIntent)
+
+//            화면 이동 후에는 로딩화면 종료
+            finish()
+        }, 2500)
+
+
     }
 }
