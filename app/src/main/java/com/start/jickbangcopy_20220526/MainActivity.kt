0e928b4c9c14eb0ea9b313b0a4175b74b4a0e396
ujin-Bang <qkddnwls14@gmail.com>
2022-05-26 16:33:57 +0900

    메인화면 주석 추가2

--- app/src/main/java/com/start/jickbangcopy_20220526/MainActivity.kt
+++ app/src/main/java/com/start/jickbangcopy_20220526/MainActivity.kt
@@ -20,14 +20,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-
+//      방데이터를 담는 변수에 RoomData추가 하기
         mRooms.add(RoomData(8000, "서울시 동대문구", 5, "1번째 방입니다"))
         mRooms.add(RoomData(18000, "서울시 서대문구", 0, "2번째 방입니다"))
         mRooms.add(RoomData(29700, "경기도 고양시", 17, "3번째 방입니다."))
         mRooms.add(RoomData(43000, "서울시 중구", -2, "4번째 방입니다."))
         mRooms.add(RoomData(175300, "서울시 송파구", 20, "5번째 방입니다."))
 
+//        RoomData추가 후 어댑터 만들어 필요한 생성자 대입
         mRoomAdapter = RoomAdapter(this,R.layout.room_list_item, mRooms)
+//        메인리스트뷰안에 어댑터에 우리가 만든 어댑터 대입 => 기본화면 나옴
         binding.roomListView.adapter = mRoomAdapter
 
     }
